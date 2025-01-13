3c5a79fd00c15759aa090f4c484b2d713fd73695
valdemir8086 <valdemirpraxedes92+github@gmail.com>
2025-01-13 02:12:39 -0300

    criação de componentes

--- src/main/java/com/jun/estudo/Date4u/Date4uApplication.java
+++ src/main/java/com/jun/estudo/Date4u/Date4uApplication.java
@@ -1,18 +1,20 @@
 package com.jun.estudo.Date4u;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.util.unit.DataSize;
+import com.jun.estudo.Date4u.core.FileSystem;
 
 @SpringBootApplication
 public class Date4uApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext appContexto = SpringApplication.run(Date4uApplication.class, args);
+	ConfigurableApplicationContext appContexto = SpringApplication.run(Date4uApplication.class, args);
+	
+	FileSystem fs = appContexto.getBean(FileSystem.class);
+	System.out.println(DataSize.ofBytes(fs.getFreeDiskSpace()).toGigabytes()+ " GB");
 		
-		Arrays.stream(appContexto.getBeanDefinitionNames()).sorted().forEach(System.out::println);
 	}
 
 }
