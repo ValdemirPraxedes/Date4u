4e3d4389b15cd66850a2e7c01fe0483041ae0c05
valdemir8086 <valdemirpraxedes92+github@gmail.com>
2025-01-19 17:27:31 -0300

    adicionado service

--- src/main/java/com/jun/estudo/Date4u/core/photo/PhotoService.java
+++ src/main/java/com/jun/estudo/Date4u/core/photo/PhotoService.java
@@ -1,5 +1,26 @@
 package com.jun.estudo.Date4u.core.photo;
 
+import java.io.UncheckedIOException;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.jun.estudo.Date4u.core.FileSystem;
+
+@Service
 public class PhotoService {
 
+	private final FileSystem fs;
+	
+	public PhotoService(FileSystem fs) {
+		this.fs = fs;
+	}
+	
+	public Optional<byte[]> download(String name) {
+		try { return Optional.of(fs.load(name+".jpg")); }
+		catch (UncheckedIOException e) {
+			return Optional.empty();
+		}
+	}
+	
 }
--- src/main/java/com/jun/estudo/Date4u/interfaces/shell/PhotoCommands.java
+++ src/main/java/com/jun/estudo/Date4u/interfaces/shell/PhotoCommands.java
@@ -1,5 +1,34 @@
 package com.jun.estudo.Date4u.interfaces.shell;
 
+import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import javax.imageio.ImageIO;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import com.jun.estudo.Date4u.core.photo.PhotoService;
+
+@ShellComponent
 public class PhotoCommands {
+	
+	private final PhotoService photoService;
+	
+	public PhotoCommands(PhotoService photoService) {
+		this.photoService = photoService;
+	}
+	
+	@ShellMethod("Show photo")
+	String showPhoto(String name) {
+		
+		return photoService.download(name).map(bytes -> {
+		try {
+			var image = ImageIO.read(new ByteArrayInputStream(bytes));
+		
+			return "Width: " + image.getWidth() + ", Height: "+ image.getHeight();
+		} catch (IOException e) {
+			return "Unable to read image dimensions";
+		}}).orElse("Image not found");
+		
+	}
 
 }
